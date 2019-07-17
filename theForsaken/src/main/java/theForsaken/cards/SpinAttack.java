afa6e0efe29670dedad6c568df4c6f9a021e17e2
arasmus8 <arasmus8@gmail.com>
2019-07-17 17:55:32 -0600

    Update Spin Attack damage 7 (10) -> 4 (5)

--- theForsaken/src/main/java/theForsaken/cards/SpinAttack.java
+++ theForsaken/src/main/java/theForsaken/cards/SpinAttack.java
@@ -28,8 +28,8 @@ public class SpinAttack extends AbstractDynamicCard {
 
     private static final int COST = 1;
 
-    private static final int DAMAGE = 7;
-    private static final int UPGRADE_PLUS_DMG = 3;
+    private static final int DAMAGE = 4;
+    private static final int UPGRADE_PLUS_DMG = 1;
     // /STAT DECLARATION/
 
     public SpinAttack() {
