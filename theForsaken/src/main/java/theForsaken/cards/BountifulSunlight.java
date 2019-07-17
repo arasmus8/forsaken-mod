64eef8ea6db28a01953532e7b29d2d8d642a2136
arasmus8 <arasmus8@gmail.com>
2019-07-17 13:48:49 -0600

    Add card Bountiful Sunlight.

---
+++ theForsaken/src/main/java/theForsaken/cards/BountifulSunlight.java
@@ -0,0 +1,63 @@
+package theForsaken.cards;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.RegenPower;
+import theForsaken.TheForsakenMod;
+import theForsaken.characters.TheForsaken;
+
+import static theForsaken.TheForsakenMod.makeCardPath;
+
+public class BountifulSunlight extends AbstractDynamicCard {
+
+    // TEXT DECLARATION
+
+    public static final String ID = TheForsakenMod.makeID(BountifulSunlight.class.getSimpleName());
+    public static final String IMG = makeCardPath("BountifulSunlight.png");
+    // Must have an image with the same NAME as the card in your image folder!
+
+    // /TEXT DECLARATION/
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = TheForsaken.Enums.COLOR_GRAY;
+
+    private static final int COST = 2;
+
+    private static final int MAGIC = 6;
+    private static final int UPGRADED_MAGIC = 3;
+
+    // /STAT DECLARATION/
+
+
+    public BountifulSunlight() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseMagicNumber = MAGIC;
+        magicNumber = baseMagicNumber;
+        exhaust = true;
+    }
+
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new RegenPower(p, magicNumber), magicNumber));
+    }
+
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(UPGRADED_MAGIC);
+            initializeDescription();
+        }
+    }
+}
