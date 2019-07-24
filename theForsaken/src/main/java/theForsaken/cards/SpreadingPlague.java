c7aa68b1132cff75ebb2be5d3206cc971ee25007
arasmus8 <arasmus8@gmail.com>
2019-07-24 11:29:51 -0600

    Add rare power Spreading Plague

---
+++ theForsaken/src/main/java/theForsaken/cards/SpreadingPlague.java
@@ -0,0 +1,63 @@
+package theForsaken.cards;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import theForsaken.TheForsakenMod;
+import theForsaken.characters.TheForsaken;
+import theForsaken.powers.SpreadingPlaguePower;
+
+import static theForsaken.TheForsakenMod.makeCardPath;
+
+public class SpreadingPlague extends AbstractDynamicCard {
+
+    // TEXT DECLARATION
+
+    public static final String ID = TheForsakenMod.makeID(SpreadingPlague.class.getSimpleName());
+    public static final String IMG = makeCardPath("SpreadingPlague.png");
+    // Must have an image with the same NAME as the card in your image folder!
+
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.RARE;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.POWER;
+    public static final CardColor COLOR = TheForsaken.Enums.COLOR_GRAY;
+
+    private static final int COST = 1;
+
+    private static final int POISON_AMT = 2;
+    private static final int UPGRADE_POISON_AMT = 2;
+
+    // /STAT DECLARATION/
+
+
+    public SpreadingPlague() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseMagicNumber = POISON_AMT;
+        magicNumber = POISON_AMT;
+    }
+
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new SpreadingPlaguePower(p, this.magicNumber)));
+    }
+
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(UPGRADE_POISON_AMT);
+            initializeDescription();
+        }
+    }
+}
---
+++ theForsaken/src/main/java/theForsaken/powers/SpreadingPlaguePower.java
@@ -0,0 +1,77 @@
+package theForsaken.powers;
+
+import basemod.interfaces.CloneablePowerInterface;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.megacrit.cardcrawl.actions.AbstractGameAction.AttackEffect;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.cards.DamageInfo.DamageType;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.powers.PoisonPower;
+import sun.security.krb5.internal.crypto.Des;
+import theForsaken.TheForsakenMod;
+import theForsaken.util.TextureLoader;
+
+import static theForsaken.TheForsakenMod.makePowerPath;
+
+
+//Reflect all incoming damage.
+
+public class SpreadingPlaguePower extends AbstractPower implements CloneablePowerInterface {
+    private static final String POWER_ID = TheForsakenMod.makeID(SpreadingPlaguePower.class.getSimpleName());
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    // We create 2 new textures *Using This Specific Texture Loader* - an 84x84 image and a 32x32 one.
+    // There's a fallback "missing texture" image, so the game shouldn't crash if you accidentally put a non-existent file.
+    private static final Texture tex84 = TextureLoader.getTexture(makePowerPath("spreading_plague84.png"));
+    private static final Texture tex32 = TextureLoader.getTexture(makePowerPath("spreading_plague32.png"));
+
+    public SpreadingPlaguePower(final AbstractCreature owner, final int amount) {
+        name = NAME;
+        ID = POWER_ID;
+
+        this.owner = owner;
+        this.amount = amount;
+
+        type = PowerType.BUFF;
+
+        // We load those textures here.
+        this.region128 = new TextureAtlas.AtlasRegion(tex84, 0, 0, 84, 84);
+        this.region48 = new TextureAtlas.AtlasRegion(tex32, 0, 0, 32, 32);
+
+        updateDescription();
+    }
+
+    @Override
+    public int onAttacked(DamageInfo info, int damageAmount) {
+        if (!(info.type == DamageType.THORNS) && !(info.type == DamageType.HP_LOSS)) {
+            AbstractCreature source = info.owner;
+            AbstractCreature target = this.owner;
+            if (source != target) {
+                this.flash();
+                AbstractDungeon.actionManager.addToTop(new ApplyPowerAction(source, target, new PoisonPower(source, target, amount), amount));
+            }
+        }
+        return damageAmount;
+    }
+
+    // Update the description when you apply this power. (i.e. add or remove an "s" in keyword(s))
+    @Override
+    public void updateDescription() {
+        description = DESCRIPTIONS[0] + amount + DESCRIPTIONS[1];
+    }
+
+    @Override
+    public AbstractPower makeCopy() {
+        return new SpreadingPlaguePower(owner, amount);
+    }
+}
