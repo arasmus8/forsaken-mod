0b8973e94b1dd512bd569727e79932667d48ea6f
arasmus8 <arasmus8@gmail.com>
2019-08-16 17:40:03 -0600

    Update damage from 3(4)x2 to 4(5)x2

--- theForsaken/src/main/java/theForsaken/cards/ScorchingSunlight.java
+++ theForsaken/src/main/java/theForsaken/cards/ScorchingSunlight.java
@@ -30,7 +30,7 @@ public class ScorchingSunlight extends AbstractDynamicCard {
 
     private static final int COST = 1;
 
-    private static final int DAMAGE = 3;
+    private static final int DAMAGE = 4;
     private static final int UPGRADE_PLUS_DMG = 1;
 
     private static final int MAGIC = 2;
