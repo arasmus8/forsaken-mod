9b81728ab16649797230df52729c8d7d86cbab33
arasmus8 <arasmus8@gmail.com>
2019-09-17 15:51:32 -0600

    Update: Rework card - OLD: Apply 9(12) Poison to ALL enemies AND yourself. NEW: Transfer your Poison to a random enemy. Unplayed: Apply 8(5) Poison to yourself and (double to) ALL enemies.

--- theForsaken/src/main/java/theForsaken/cards/CorruptedWord.java
+++ theForsaken/src/main/java/theForsaken/cards/CorruptedWord.java
@@ -2,8 +2,12 @@ package theForsaken.cards;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction.AttackEffect;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.cards.CardQueueItem;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.PoisonPower;
 import theForsaken.CustomTags;
@@ -19,7 +23,9 @@ public class CorruptedWord extends AbstractDynamicCard {
 
     public static final String ID = TheForsakenMod.makeID(CorruptedWord.class.getSimpleName());
     public static final String IMG = makeCardPath("CorruptedWord.png");
-    // Must have an image with the same NAME as the card in your image folder!
+    // Must have an image with the same NAME as the card in your image folder!\
+    private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
+    private static final String UPGRADE_DESCRIPTION = CARD_STRINGS.UPGRADE_DESCRIPTION;
 
     // /TEXT DECLARATION/
 
@@ -32,8 +38,8 @@ public class CorruptedWord extends AbstractDynamicCard {
 
     private static final int COST = 1;
 
-    private static final int MAGIC = 9;
-    private static final int UPGRADED_MAGIC = 3;
+    private static final int MAGIC = 8;
+    private static final int UPGRADED_MAGIC = -2;
 
     // /STAT DECLARATION/
 
@@ -49,17 +55,40 @@ public class CorruptedWord extends AbstractDynamicCard {
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new PoisonPower(p, p, magicNumber), magicNumber, AttackEffect.POISON));
-        for (AbstractMonster monster:AbstractDungeon.getCurrRoom().monsters.monsters) {
-            AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(monster, p, new PoisonPower(monster, p, magicNumber), magicNumber, AttackEffect.POISON));
-        }
-        if(p.hasRelic(PlagueMask.ID)) {
-            p.getRelic(PlagueMask.ID).flash();
-            AbstractMonster mo = AbstractDungeon.getRandomMonster();
-            AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(mo, p, new PoisonPower(mo, p, magicNumber), magicNumber, AttackEffect.POISON));
+        if (this.dontTriggerOnUseCard) {
+            AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new PoisonPower(p, p, magicNumber), magicNumber, AttackEffect.POISON));
+            int amount = magicNumber;
+            if (upgraded) {
+                amount *= 2;
+            }
+            for (AbstractMonster monster : AbstractDungeon.getCurrRoom().monsters.monsters) {
+                AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(monster, p, new PoisonPower(monster, p, amount), amount, AttackEffect.POISON));
+            }
+            if (p.hasRelic(PlagueMask.ID)) {
+                p.getRelic(PlagueMask.ID).flash();
+                AbstractMonster mo = AbstractDungeon.getRandomMonster();
+                if (mo != null) {
+                    AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(mo, p, new PoisonPower(mo, p, magicNumber), magicNumber, AttackEffect.POISON));
+                }
+            }
+        } else if (p != null) {
+            if (p.hasPower(PoisonPower.POWER_ID)) {
+                int amount = p.getPower(PoisonPower.POWER_ID).amount;
+                AbstractDungeon.actionManager.addToBottom(new RemoveSpecificPowerAction(p, p, PoisonPower.POWER_ID));
+                AbstractMonster mo = AbstractDungeon.getRandomMonster();
+                if (mo != null) {
+                    AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(mo, p, new PoisonPower(mo, p, amount), amount, AttackEffect.POISON));
+                }
+            }
         }
     }
 
+    @Override
+    public void triggerOnEndOfTurnForPlayingCard() {
+        this.dontTriggerOnUseCard = true;
+        this.freeToPlayOnce = true;
+        AbstractDungeon.actionManager.cardQueue.add(new CardQueueItem(this, true));
+    }
 
     // Upgraded stats.
     @Override
@@ -67,6 +96,7 @@ public class CorruptedWord extends AbstractDynamicCard {
         if (!upgraded) {
             upgradeName();
             upgradeMagicNumber(UPGRADED_MAGIC);
+            rawDescription = UPGRADE_DESCRIPTION;
             initializeDescription();
         }
     }
