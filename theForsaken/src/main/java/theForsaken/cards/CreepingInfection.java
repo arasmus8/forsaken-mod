7f11b4ed979ccfbc9d2cffb78368d0dcdd751316
arasmus8 <arasmus8@gmail.com>
2019-09-17 15:54:41 -0600

    Update - rework card: OLD: 0 - Apply 3(4) Poison. When drawn, add a copy to your hand. Exhaust NEW: 1(0) - Reduce your Poison by 3(4) and heal the same amount. Unplayed: Apply 3(4) Poison to ALL enemies AND yourself, and add a copy to your draw pile.

--- theForsaken/src/main/java/theForsaken/cards/CreepingInfection.java
+++ theForsaken/src/main/java/theForsaken/cards/CreepingInfection.java
@@ -1,13 +1,18 @@
 package theForsaken.cards;
 
+import com.megacrit.cardcrawl.actions.AbstractGameAction.AttackEffect;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
+import com.megacrit.cardcrawl.actions.common.HealAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInDrawPileAction;
+import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
+import com.megacrit.cardcrawl.cards.CardQueueItem;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.PoisonPower;
 import theForsaken.TheForsakenMod;
 import theForsaken.characters.TheForsaken;
+import theForsaken.relics.PlagueMask;
 
 import static theForsaken.TheForsakenMod.makeCardPath;
 
@@ -24,11 +29,12 @@ public class CreepingInfection extends AbstractDynamicCard {
     // STAT DECLARATION
 
     private static final CardRarity RARITY = CardRarity.COMMON;
-    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardTarget TARGET = CardTarget.SELF;
     private static final CardType TYPE = CardType.SKILL;
     public static final CardColor COLOR = TheForsaken.Enums.COLOR_GOLD;
 
-    private static final int COST = 0;
+    private static final int COST = 1;
+    private static final int UPGRADE_COST = 0;
 
     private static final int MAGIC = 3;
     private static final int UPGRADED_MAGIC = 1;
@@ -47,12 +53,36 @@ public class CreepingInfection extends AbstractDynamicCard {
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(m, p, new PoisonPower(m, p, magicNumber), magicNumber));
+        if (this.dontTriggerOnUseCard) {
+            int amount = magicNumber;
+            AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new PoisonPower(p, p, amount), amount, AttackEffect.POISON));
+            for (AbstractMonster monster : AbstractDungeon.getCurrRoom().monsters.monsters) {
+                AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(monster, p, new PoisonPower(monster, p, amount), amount, AttackEffect.POISON));
+            }
+            if (p.hasRelic(PlagueMask.ID)) {
+                p.getRelic(PlagueMask.ID).flash();
+                AbstractMonster mo = AbstractDungeon.getRandomMonster();
+                if (mo != null) {
+                    AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(mo, p, new PoisonPower(mo, p, amount), amount, AttackEffect.POISON));
+                }
+            }
+            AbstractDungeon.actionManager.addToBottom(new MakeTempCardInDrawPileAction(this.makeStatEquivalentCopy(), 1, true, true));
+        } else {
+            if (p.hasPower(PoisonPower.POWER_ID)) {
+                int amount = p.getPower(PoisonPower.POWER_ID).amount;
+                if (amount > 0) {
+                    AbstractDungeon.actionManager.addToBottom(new ReducePowerAction(p, p, PoisonPower.POWER_ID, magicNumber));
+                    AbstractDungeon.actionManager.addToBottom(new HealAction(p, p, amount));
+                }
+            }
+        }
     }
 
     @Override
-    public void triggerWhenDrawn() {
-        AbstractDungeon.actionManager.addToTop(new MakeTempCardInHandAction(this.makeStatEquivalentCopy()));
+    public void triggerOnEndOfTurnForPlayingCard() {
+        this.dontTriggerOnUseCard = true;
+        this.freeToPlayOnce = true;
+        AbstractDungeon.actionManager.cardQueue.add(new CardQueueItem(this, true));
     }
 
     // Upgraded stats.
@@ -60,6 +90,7 @@ public class CreepingInfection extends AbstractDynamicCard {
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
+            upgradeBaseCost(UPGRADE_COST);
             upgradeMagicNumber(UPGRADED_MAGIC);
             initializeDescription();
         }
