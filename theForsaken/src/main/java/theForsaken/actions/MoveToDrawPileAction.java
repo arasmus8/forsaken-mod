7e1cd8aabf2b5b8ab968cb39339ff09caa75c9f0
arasmus8 <arasmus8@gmail.com>
2019-09-19 14:59:37 -0600

    Update - Buff Temperance. Now gets shuffled into draw pile if unplayed.
    (cherry picked from commit 5b9d07964a7664f769e72a307e288f029937432d)

---
+++ theForsaken/src/main/java/theForsaken/actions/MoveToDrawPileAction.java
@@ -0,0 +1,28 @@
+package theForsaken.actions;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import theForsaken.TheForsakenMod;
+
+public class MoveToDrawPileAction extends AbstractGameAction {
+    public static final String ID = TheForsakenMod.makeID(MoveToDrawPileAction.class.getSimpleName());
+    private AbstractPlayer p;
+    private AbstractCard card;
+
+    public MoveToDrawPileAction(AbstractCard card) {
+        this.p = AbstractDungeon.player;
+        this.card = card;
+        this.duration = Settings.ACTION_DUR_FAST;
+        this.actionType = ActionType.CARD_MANIPULATION;
+    }
+
+    public void update() {
+        if (this.duration == Settings.ACTION_DUR_FAST) {
+            p.hand.moveToDeck(card, true);
+            isDone = true;
+        }
+    }
+}
