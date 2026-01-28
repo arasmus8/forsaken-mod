a89b6354448df33a88ed3dd42090c7b70ad0b86f
arasmus8 <arasmus8@gmail.com>
2026-01-28 16:04:58 -0700

    wording improvements.

--- theForsaken/src/main/java/forsaken/cards/attacks/ShieldCharge.java
+++ theForsaken/src/main/java/forsaken/cards/attacks/ShieldCharge.java
@@ -1,10 +1,14 @@
 package forsaken.cards.attacks;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.vfx.combat.FlashAtkImgEffect;
 import forsaken.TheForsakenMod;
+import forsaken.actions.FunctionalAction;
 import forsaken.cards.AbstractQuickdrawCard;
 
 @SuppressWarnings("unused")
@@ -14,15 +18,25 @@ public class ShieldCharge extends AbstractQuickdrawCard {
     public ShieldCharge() {
         super(ID, CardType.ATTACK, CardRarity.COMMON, CardTarget.ENEMY);
         damage = baseDamage = 4;
-        upgradeDamageBy = 1;
+        upgradeDamageBy = 2;
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         applyPowers();
-        dealDamage(m, AbstractGameAction.AttackEffect.BLUNT_LIGHT);
-        if (upgraded) {
-            qAction(new DrawCardAction(1));
-        }
+        qAction(new FunctionalAction(firstUpdate -> {
+            if (m != null) {
+                DamageInfo info = makeDamageInfo(damage, damageTypeForTurn);
+                AbstractDungeon.effectList.add(new FlashAtkImgEffect(m.hb.cX, m.hb.cY, AbstractGameAction.AttackEffect.BLUNT_LIGHT, false));
+                m.damage(info);
+                if (upgraded) {
+                    int block = Math.max(0, m.lastDamageTaken);
+                    if (block > 0) {
+                        addToBot(new GainBlockAction(p, p, block));
+                    }
+                }
+            }
+            return true;
+        }));
     }
 }
